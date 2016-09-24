ab9c35dc653f88bcc880745c30276c14360e93fb
PaKowalski <pkowalski@cambridge-intl.com>
2016-09-23 22:39:29 -0400

    Finish Barebones Settings

--- PKSRSRoutine/PKSRSRoutine/PKSRSRoutine.cs
+++ PKSRSRoutine/PKSRSRoutine/PKSRSRoutine.cs
@@ -1,16 +1,242 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using log4net;
 using Loki.Bot;
+using Loki.Bot.Pathfinding;
 using Loki.Common;
+using Loki.Game;
+using Loki.Game.Objects;
 
 namespace PKSRSRoutine
 {
     public class PKSRSRoutine : IRoutine
     {
+        #region Temp Compatibility 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="distanceFromPoint"></param>
+        /// <param name="dontLeaveFrame">Should the current frame not be left?</param>
+        /// <returns></returns>
+        public static int NumberOfMobsBetween(NetworkObject start, NetworkObject end, int distanceFromPoint = 5,
+            bool dontLeaveFrame = false)
+        {
+            var mobs = LokiPoe.ObjectManager.GetObjectsByType<Monster>().Where(d => d.IsActive).ToList();
+            if (!mobs.Any())
+                return 0;
+
+            var path = ExilePather.GetPointsOnSegment(start.Position, end.Position, dontLeaveFrame);
+
+            var count = 0;
+            for (var i = 0; i < path.Count; i += 10)
+            {
+                foreach (var mob in mobs)
+                {
+                    if (mob.Position.Distance(path[i]) <= distanceFromPoint)
+                    {
+                        ++count;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// Checks for a closed door between start and end.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="distanceFromPoint">How far to check around each point for a door object.</param>
+        /// <param name="stride">The distance between points to check in the path.</param>
+        /// <param name="dontLeaveFrame">Should the current frame not be left?</param>
+        /// <returns>true if there's a closed door and false otherwise.</returns>
+        public static bool ClosedDoorBetween(NetworkObject start, NetworkObject end, int distanceFromPoint = 10,
+            int stride = 10, bool dontLeaveFrame = false)
+        {
+            return ClosedDoorBetween(start.Position, end.Position, distanceFromPoint, stride, dontLeaveFrame);
+        }
+
+        /// <summary>
+        /// Checks for a closed door between start and end.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="distanceFromPoint">How far to check around each point for a door object.</param>
+        /// <param name="stride">The distance between points to check in the path.</param>
+        /// <param name="dontLeaveFrame">Should the current frame not be left?</param>
+        /// <returns>true if there's a closed door and false otherwise.</returns>
+        public static bool ClosedDoorBetween(NetworkObject start, Vector2i end, int distanceFromPoint = 10,
+            int stride = 10, bool dontLeaveFrame = false)
+        {
+            return ClosedDoorBetween(start.Position, end, distanceFromPoint, stride, dontLeaveFrame);
+        }
+
+        /// <summary>
+        /// Checks for a closed door between start and end.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="distanceFromPoint">How far to check around each point for a door object.</param>
+        /// <param name="stride">The distance between points to check in the path.</param>
+        /// <param name="dontLeaveFrame">Should the current frame not be left?</param>
+        /// <returns>true if there's a closed door and false otherwise.</returns>
+        public static bool ClosedDoorBetween(Vector2i start, NetworkObject end, int distanceFromPoint = 10,
+            int stride = 10, bool dontLeaveFrame = false)
+        {
+            return ClosedDoorBetween(start, end.Position, distanceFromPoint, stride, dontLeaveFrame);
+        }
+
+        /// <summary>
+        /// Checks for a closed door between start and end.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="distanceFromPoint">How far to check around each point for a door object.</param>
+        /// <param name="stride">The distance between points to check in the path.</param>
+        /// <param name="dontLeaveFrame">Should the current frame not be left?</param>
+        /// <returns>true if there's a closed door and false otherwise.</returns>
+        public static bool ClosedDoorBetween(Vector2i start, Vector2i end, int distanceFromPoint = 10, int stride = 10,
+            bool dontLeaveFrame = false)
+        {
+            var doors = LokiPoe.ObjectManager.Doors.Where(d => !d.IsOpened).ToList();
+
+            if (!doors.Any())
+                return false;
+
+            var path = ExilePather.GetPointsOnSegment(start, end, dontLeaveFrame);
+
+            for (var i = 0; i < path.Count; i += stride)
+            {
+                foreach (var door in doors)
+                {
+                    if (door.Position.Distance(path[i]) <= distanceFromPoint)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Returns the number of mobs near a target.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="distance"></param>
+        /// <param name="dead"></param>
+        /// <returns></returns>
+        public static int NumberOfMobsNear(NetworkObject target, float distance, bool dead = false)
+        {
+            var mpos = target.Position;
+
+            var curCount = 0;
+
+            foreach (var mob in LokiPoe.ObjectManager.Objects.OfType<Monster>())
+            {
+                if (mob.Id == target.Id)
+                {
+                    continue;
+                }
+
+                // If we're only checking for dead mobs... then... yeah...
+                if (dead)
+                {
+                    if (!mob.IsDead)
+                    {
+                        continue;
+                    }
+                }
+                else if (!mob.IsActive)
+                {
+                    continue;
+                }
+
+                if (mob.Position.Distance(mpos) < distance)
+                {
+                    curCount++;
+                }
+            }
+
+            return curCount;
+        }
+
+        #endregion
+
+        private static readonly ILog Log = Logger.GetLoggerInstanceForType();
+
+        // Auto-set, you do not have to change these.
+        private int _sgSlot;
+        private int _summonChaosGolemSlot = -1;
+        private int _summonIceGolemSlot = -1;
+        private int _summonFlameGolemSlot = -1;
+        private int _summonStoneGolemSlot = -1;
+        private int _summonLightningGolemSlot = -1;
+        private int _raiseZombieSlot = -1;
+        private int _raiseSpectreSlot = -1;
+        private int _animateWeaponSlot = -1;
+        private int _animateGuardianSlot = -1;
+        private int _flameblastSlot = -1;
+        private int _enduringCrySlot = -1;
+        private int _moltenShellSlot = -1;
+        private int _bloodRageSlot = -1;
+        private int _rfSlot = -1;
+        private readonly List<int> _curseSlots = new List<int>();
+        private int _auraSlot = -1;
+        private int _totemSlot = -1;
+        private int _trapSlot = -1;
+        private int _mineSlot = -1;
+        private int _summonSkeletonsSlot = -1;
+        private int _summonRagingSpiritSlot = -1;
+        private int _coldSnapSlot = -1;
+        private int _contagionSlot = -1;
+        private int _witherSlot = -1;
+        private int _bladeVortexSlot = -1;
+        private int _bladeVortexCount = 0;
+
+        private bool _isCasting;
+        private int _castingSlot;
+
+        private int _currentLeashRange = -1;
+
+
+        private readonly Stopwatch _trapStopwatch = Stopwatch.StartNew();
+        private readonly Stopwatch _totemStopwatch = Stopwatch.StartNew();
+        private readonly Stopwatch _mineStopwatch = Stopwatch.StartNew();
+        private readonly Stopwatch _animateWeaponStopwatch = Stopwatch.StartNew();
+        private readonly Stopwatch _animateGuardianStopwatch = Stopwatch.StartNew();
+        private readonly Stopwatch _moltenShellStopwatch = Stopwatch.StartNew();
+        private readonly List<int> _ignoreAnimatedItems = new List<int>();
+        private readonly Stopwatch _vaalStopwatch = Stopwatch.StartNew();
+
+        private int _summonSkeletonCount;
+        private readonly Stopwatch _summonSkeletonsStopwatch = Stopwatch.StartNew();
+
+        private readonly Stopwatch _summonGolemStopwatch = Stopwatch.StartNew();
+
+        private int _summonRagingSpiritCount;
+        private readonly Stopwatch _summonRagingSpiritStopwatch = Stopwatch.StartNew();
+
+        private bool _castingFlameblast;
+        private int _lastFlameblastCharges;
+        private bool _needsUpdate;
+
+        private readonly Targeting _combatTargeting = new Targeting();
+
+        private Dictionary<string, Func<dynamic[], object>> _exposedSettings;
+
+
+
+        #region IRoutine Implementations
         public UserControl Control { get; }
         public JsonSettings Settings { get; }
         public void Start()
@@ -51,5 +277,6 @@ namespace PKSRSRoutine
         {
             throw new NotImplementedException();
         }
+#endregion
     }
 }
--- PKSRSRoutine/PKSRSRoutine/PKSRSRoutineSettings.cs
+++ PKSRSRoutine/PKSRSRoutine/PKSRSRoutineSettings.cs
@@ -1,12 +1,393 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Loki;
+using Loki.Common;
+using Newtonsoft.Json;
 
 namespace PKSRSRoutine
 {
-    class PKSRSRoutineSettings
+    public class PKSRSRoutineSettings : JsonSettings
     {
+        private static PKSRSRoutineSettings _instance;
+
+        /// <summary>The current instance for this class. </summary>
+        public static PKSRSRoutineSettings Instance
+        {
+            get { return _instance ?? (_instance = new PKSRSRoutineSettings()); }
+        }
+
+        /// <summary>The default ctor. Will use the settings path "VortexRoutine".</summary>
+        public PKSRSRoutineSettings()
+            : base(GetSettingsFilePath(Configuration.Instance.Name, string.Format("{0}.json", "PKSRSRoutine")))
+        {
+        }
+
+        private int _singleTargetMeleeSlot;
+        private int _singleTargetRangedSlot;
+        private int _aoeMeleeSlot;
+        private int _aoeRangedSlot;
+        private int _fallbackSlot;
+        private int _combatRange;
+        private int _maxMeleeRange;
+        private int _maxRangeRange;
+        private bool _alwaysAttackInPlace;
+
+        private int _totemDelayMs;
+
+        private int _summonSkeletonCountPerDelay;
+        private int _summonSkeletonDelayMs;
+
+        private int _summonRagingSpiritCountPerDelay;
+        private int _summonRagingSpiritDelayMs;
+
+        private bool _autoCastVaalSkills;
+
+        private bool _enableAurasFromItems;
+
+        private bool _debugAuras;
+
+        private bool _leaveFrame;
+
+        private string _blacklistedSkillIds;
+
+        private bool _skipShrines;
+
+
+        /// <summary>
+		/// Should the CR leave the current frame to do pathfinds and other frame intensive tasks.
+		/// NOTE: This might cause random memory exceptions due to memory no longer being valid in the CR.
+		/// </summary>
+		[DefaultValue(false)]
+        public bool LeaveFrame
+        {
+            get { return _leaveFrame; }
+            set
+            {
+                if (value.Equals(_leaveFrame))
+                {
+                    return;
+                }
+                _leaveFrame = value;
+                NotifyPropertyChanged(() => LeaveFrame);
+            }
+        }
+
+        /// <summary>
+		/// Should the CR skip shrines?
+		/// </summary>
+		[DefaultValue(false)]
+        public bool SkipShrines
+        {
+            get { return _skipShrines; }
+            set
+            {
+                if (value.Equals(_skipShrines))
+                {
+                    return;
+                }
+                _skipShrines = value;
+                NotifyPropertyChanged(() => SkipShrines);
+            }
+        }
+
+        /// <summary>
+        /// Should the CR use auras granted by items rather than skill gems?
+        /// </summary>
+        [DefaultValue(true)]
+        public bool EnableAurasFromItems
+        {
+            get { return _enableAurasFromItems; }
+            set
+            {
+                if (value.Equals(_enableAurasFromItems))
+                {
+                    return;
+                }
+                _enableAurasFromItems = value;
+                NotifyPropertyChanged(() => EnableAurasFromItems);
+            }
+        }
+
+        /// <summary>
+        /// Should the CR output casting errors for auras?
+        /// </summary>
+        [DefaultValue(false)]
+        public bool DebugAuras
+        {
+            get { return _debugAuras; }
+            set
+            {
+                if (value.Equals(_debugAuras))
+                {
+                    return;
+                }
+                _debugAuras = value;
+                NotifyPropertyChanged(() => DebugAuras);
+            }
+        }
+
+        /// <summary>
+        /// Should vaal skills be auto-cast during combat.
+        /// </summary>
+        [DefaultValue(true)]
+        public bool AutoCastVaalSkills
+        {
+            get { return _autoCastVaalSkills; }
+            set
+            {
+                if (value.Equals(_autoCastVaalSkills))
+                {
+                    return;
+                }
+                _autoCastVaalSkills = value;
+                NotifyPropertyChanged(() => AutoCastVaalSkills);
+            }
+        }
+
+        /// <summary>
+        /// How many casts to perform before the delay happens.
+        /// </summary>
+        [DefaultValue(3)]
+        public int SummonRagingSpiritCountPerDelay
+        {
+            get { return _summonRagingSpiritCountPerDelay; }
+            set
+            {
+                if (value.Equals(_summonRagingSpiritCountPerDelay))
+                {
+                    return;
+                }
+                _summonRagingSpiritCountPerDelay = value;
+                NotifyPropertyChanged(() => SummonRagingSpiritCountPerDelay);
+            }
+        }
+
+        /// <summary>
+        /// How long should the CR wait after performing all the casts.
+        /// </summary>
+        [DefaultValue(5000)]
+        public int SummonRagingSpiritDelayMs
+        {
+            get { return _summonRagingSpiritDelayMs; }
+            set
+            {
+                if (value.Equals(_summonRagingSpiritDelayMs))
+                {
+                    return;
+                }
+                _summonRagingSpiritDelayMs = value;
+                NotifyPropertyChanged(() => SummonRagingSpiritDelayMs);
+            }
+        }
+
+        /// <summary>
+        /// Should the CR always attack in place.
+        /// </summary>
+        [DefaultValue(false)]
+        public bool AlwaysAttackInPlace
+        {
+            get { return _alwaysAttackInPlace; }
+            set
+            {
+                if (value.Equals(_alwaysAttackInPlace))
+                {
+                    return;
+                }
+                _alwaysAttackInPlace = value;
+                NotifyPropertyChanged(() => AlwaysAttackInPlace);
+            }
+        }
+
+        /// <summary>
+        /// The skill slot to use in melee range.
+        /// </summary>
+        [DefaultValue(-1)]
+        public int SingleTargetMeleeSlot
+        {
+            get { return _singleTargetMeleeSlot; }
+            set
+            {
+                if (value.Equals(_singleTargetMeleeSlot))
+                {
+                    return;
+                }
+                _singleTargetMeleeSlot = value;
+                NotifyPropertyChanged(() => SingleTargetMeleeSlot);
+            }
+        }
+
+        /// <summary>
+        /// The skill slot to use outside of melee range.
+        /// </summary>
+        [DefaultValue(-1)]
+        public int SingleTargetRangedSlot
+        {
+            get { return _singleTargetRangedSlot; }
+            set
+            {
+                if (value.Equals(_singleTargetRangedSlot))
+                {
+                    return;
+                }
+                _singleTargetRangedSlot = value;
+                NotifyPropertyChanged(() => SingleTargetRangedSlot);
+            }
+        }
+
+        /// <summary>
+        /// The skill slot to use in melee range.
+        /// </summary>
+        [DefaultValue(-1)]
+        public int AoeMeleeSlot
+        {
+            get { return _aoeMeleeSlot; }
+            set
+            {
+                if (value.Equals(_aoeMeleeSlot))
+                {
+                    return;
+                }
+                _aoeMeleeSlot = value;
+                NotifyPropertyChanged(() => AoeMeleeSlot);
+            }
+        }
+
+        /// <summary>
+        /// The skill slot to use outside of melee range.
+        /// </summary>
+        [DefaultValue(-1)]
+        public int AoeRangedSlot
+        {
+            get { return _aoeRangedSlot; }
+            set
+            {
+                if (value.Equals(_aoeRangedSlot))
+                {
+                    return;
+                }
+                _aoeRangedSlot = value;
+                NotifyPropertyChanged(() => AoeRangedSlot);
+            }
+        }
+
+        /// <summary>
+        /// The skill slot to use as a fallback if the desired skill cannot be cast.
+        /// </summary>
+        [DefaultValue(-1)]
+        public int FallbackSlot
+        {
+            get { return _fallbackSlot; }
+            set
+            {
+                if (value.Equals(_fallbackSlot))
+                {
+                    return;
+                }
+                _fallbackSlot = value;
+                NotifyPropertyChanged(() => FallbackSlot);
+            }
+        }
+
+        /// <summary>
+        /// Only attack mobs within this range.
+        /// </summary>
+        [DefaultValue(70)]
+        public int CombatRange
+        {
+            get { return _combatRange; }
+            set
+            {
+                if (value.Equals(_combatRange))
+                {
+                    return;
+                }
+                _combatRange = value;
+                NotifyPropertyChanged(() => CombatRange);
+            }
+        }
+
+        /// <summary>
+        /// How close does a mob need to be to trigger the Melee skill.
+        /// Do not set too high, as the cursor will overlap the GUI.
+        /// </summary>
+        [DefaultValue(10)]
+        public int MaxMeleeRange
+        {
+            get { return _maxMeleeRange; }
+            set
+            {
+                if (value.Equals(_maxMeleeRange))
+                {
+                    return;
+                }
+                _maxMeleeRange = value;
+                NotifyPropertyChanged(() => MaxMeleeRange);
+            }
+        }
+
+        /// <summary>
+        /// How close does a mob need to be to trigger the Ranged skill.
+        /// Do not set too high, as the cursor will overlap the GUI.
+        /// </summary>
+        [DefaultValue(35)]
+        public int MaxRangeRange
+        {
+            get { return _maxRangeRange; }
+            set
+            {
+                if (value.Equals(_maxRangeRange))
+                {
+                    return;
+                }
+                _maxRangeRange = value;
+                NotifyPropertyChanged(() => MaxRangeRange);
+            }
+        }
+
+        /// <summary>
+        /// The delay between casting totems in combat.
+        /// </summary>
+        [DefaultValue(5000)]
+        public int TotemDelayMs
+        {
+            get { return _totemDelayMs; }
+            set
+            {
+                if (value.Equals(_totemDelayMs))
+                {
+                    return;
+                }
+                _totemDelayMs = value;
+                NotifyPropertyChanged(() => TotemDelayMs);
+            }
+        }
+
+        [JsonIgnore]
+        private List<int> _allSkillSlots;
+
+        /// <summary> </summary>
+        [JsonIgnore]
+        public List<int> AllSkillSlots
+        {
+            get
+            {
+                return _allSkillSlots ?? (_allSkillSlots = new List<int>
+                {
+                    -1,
+                    1,
+                    2,
+                    3,
+                    4,
+                    5,
+                    6,
+                    7,
+                    8
+                });
+            }
+        }
     }
 }
